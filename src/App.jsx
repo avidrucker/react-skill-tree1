ddd338bcf1ebac2ecaad14b93159bc735efb84e3
avidrucker <avidrucker@gmail.com>
2024-10-17 12:53:09 -0400

    feat info modal hidden state, missing description display, img display

--- src/App.jsx
+++ src/App.jsx
@@ -108,17 +108,30 @@ DescriptionModal.propTypes = {
 function InfoModal({ nodeData, onClose }) {
   if (!nodeData) return null;
 
+  let { label, image, description, state } = nodeData;
+  if (state === HIDDEN_STATE) {
+    label = "???";
+    image = "";
+    description = "This node is currently hidden. You must unlock it to view its description.";
+  }
+
   return (
     <div
-      className="info-panel absolute left-0 top-0 w-100 h-100 bg-black-40 white pa4 tc bg-blur"
+      className="info-panel absolute left-0 top-0 w-100 h-100 bg-black-40 white tc bg-blur"
     >
       <div className="w-100 h-100 absolute o-0 left-0 top-0" onClick={onClose}>  
         Click here to close
       </div>
-      <h2 className="relative z-1 f1 old-english-text-mt ma0 dib">{nodeData.label}</h2>
+      <h2 className="relative z-1 f1 old-english-text-mt ma0 dib mt4">{label}</h2>
+      <br/>
+      <div className="absolute w-100 z-1 h-75 pa4 pointer-events-none">
+        <img className="o-30 h-100" src={image} alt={label} />
+      </div>
       {
-        nodeData.description &&
-        <p className="relative f4 z-1 pa3 tl lh-copy measure mr-auto ml-auto h-75 overflow-y-auto">{nodeData.description}</p>
+        description ?
+        <div className="pointer-events-none relative dib ma0 pa4 f4 z-1 tl lh-copy measure mr-auto ml-auto h-75 overflow-y-auto">
+          <p className="pointer-events-auto ma0">{description}</p></div> :
+        <p className="relative dib ma0 mt4 f4 dib z-1 tc lh-copy mr-auto ml-auto">No description available.</p>
       }
     </div>
   );
@@ -127,6 +140,8 @@ function InfoModal({ nodeData, onClose }) {
 InfoModal.propTypes = {
   nodeData: PropTypes.shape({
     label: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
   }),
   onClose: PropTypes.func.isRequired,
