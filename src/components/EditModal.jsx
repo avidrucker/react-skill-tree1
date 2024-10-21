3f93bc817a240c719bd5efa7d3d4b9d7b52ed9a3
avidrucker <avidrucker@gmail.com>
2024-10-21 12:34:22 -0400

    update modal height to vh-50, set btns to font weight 600, dim by default, glow on hover

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -9,16 +9,15 @@ function EditModal({ isOpen, onClose, onSave, description, label, onDescriptionC
       <div className="modal-content">
         <h2 className="ma0 mt4 mb3">{`Edit Info Text for ${label}`}</h2>
         <textarea
-          className="f5"
+          className="f5 vh-50"
           value={description}
           onChange={(e) => onDescriptionChange(e.target.value)}
-          rows={15}
           cols={50}
         />
         <p className="ma0 f5">{`Info text is viewable by clicking on items in 'Player Mode'.`}</p>
-        <div className="mt3">
-          <button className="mr2 glow-bg ba b--white bw1 br4" onClick={onSave}>Save</button>
-          <button className="glow-bg ba b--white bw1 br4" onClick={onClose}>Close</button>
+        <div className="mt2">
+          <button className="mr2 glow-bg ba b--white bw1 br4 fw6 o-50 glow" onClick={onSave}>Save</button>
+          <button className="glow-bg ba b--white bw1 br4 fw6 o-50 glow" onClick={onClose}>Close</button>
         </div>
       </div>
     </div>
