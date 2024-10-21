113de82e12e6fc315bf281edd91b49833a60b4bc
avidrucker <avidrucker@gmail.com>
2024-10-21 10:31:08 -0400

    update modify edit modal font size, add info p element

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -9,11 +9,13 @@ function EditModal({ isOpen, onClose, onSave, description, label, onDescriptionC
       <div className="modal-content">
         <h2 className="ma0 mt4 mb3">{`Edit Info Text for ${label}`}</h2>
         <textarea
+          className="f5"
           value={description}
           onChange={(e) => onDescriptionChange(e.target.value)}
           rows={15}
           cols={50}
         />
+        <p className="ma0 f5">{`Info text is viewable by clicking on items in 'Player Mode'.`}</p>
         <div className="mt3">
           <button className="mr2 glow-bg ba b--white bw1 br4" onClick={onSave}>Save</button>
           <button className="glow-bg ba b--white bw1 br4" onClick={onClose}>Close</button>
